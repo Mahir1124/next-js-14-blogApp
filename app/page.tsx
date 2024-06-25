d58088a549670f357f4c8b12ccadc5acca904345
mahirkhan <mahirkhang1124@gmail.com>
2024-06-25 20:59:17 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,48 @@
+import { client, urlFor } from '@/app/lib/sanity'
+import { simpleBlogCard } from './lib/interface';
+import { Card, CardContent } from '@/components/ui/card';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+async function getData() {
+  const query = `
+  *[_type == 'blog'] | order(_createdAt desc) {
+  title,
+    smallDescription,
+    "currentSlug": slug.current ,
+    titleImage
+}
+  `
+  const data = await client.fetch(query);
+  console.log(data)
+  return data;
+}
+export default async function Home() {
+  const data: simpleBlogCard[] = await getData();
+  console.log(data)
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 mt-5 gap-5 '>
+      {
+        data.map((post , idx)=>(
+          <Card key={idx}>
+            <Image src={urlFor(post.titleImage).url()}  alt='image' width={500} height={500}
+            className='rounded-t-lg h-[200px] object-cover'/>
+            <CardContent className='mt-3'>
+              <h3 className='text-lg line-clamp-2 font-bold'>{post.title}</h3>
+              <p className=' line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300'>{post.smallDescription}
+              </p>
+              <Button asChild className='w-full mt-7 bg-blue-800 '>
+                <Link href={`/blog/${post.currentSlug}`}>
+                Read More
+                </Link>
+              </Button>
+
+            </CardContent>
+          </Card>
+        ))
+      }
+                                  
+    
+    </div>
+  );
+}
---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'zwi9iolw',
+    dataset: 'production'
+  }
+})
